ec9790e7bcea01b31072d758af3d9e4193f3d979
Areef Syed <areefsyed96@gmail.com>
2023-02-20 22:25:59 +0530

    Working on SSR

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,17 +1,25 @@
+import Head from "next/head";
 import User from "../../components/user";
 
 function UserList({ users }) {
   return (
     <>
+      <Head>
+        <meta property="og:title" content="Title test SEO" />
+        <meta property={users[0].name} content={users[0].email} />
+      </Head>
       <h2>Users</h2>
-      <User users={users} />
+      <p>{users[0].id}</p>
+      <p>{users[0].email}</p>
+      <p>{users[0].name}</p>
+      {/* <User users={users} /> */}
     </>
   );
 }
 
 export default UserList;
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await response.json();
   return {
